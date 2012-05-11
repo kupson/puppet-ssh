50f0c6e5cda81e0c9f0f0bf79b34139942627d27
Rafał Kupka <kupson@kupson.net>
2012-05-11 14:34:16 +0200

    Place authorized keys in /etc/ssh/authorized_keys/
    Also group augeas resources.

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,6 +7,7 @@ class ssh::params {
             $service_name         = 'ssh'
             $path_ssh_known_hosts = '/etc/ssh/ssh_known_hosts'
             $path_sshd_config     = '/etc/ssh/sshd_config'
+            $path_authorized_keys = '/etc/ssh/authorized_keys'
         }
         default: {
             fail("Unsupported platform: ${::operatingsystem}")
--- manifests/server.pp
+++ manifests/server.pp
@@ -15,25 +15,33 @@ class ssh::server {
             require    => Package[$ssh::params::package_server];
     }
 
-    Augeas {
-        lens    => 'Sshd.lns',
-        context => "/files${ssh::params::path_sshd_config}",
-        incl    => $ssh::params::path_sshd_config,
-        notify  => Service[$ssh::params::service_name],
+    file {
+        $ssh::params::path_authorized_keys:
+            ensure  => directory,
+            owner   => 'root',
+            group   => 'root',
+            mode    => 0711,
+            require => Package[$ssh::params::package_server];
     }
 
     augeas {
-        'sshd_protocol':      changes => 'set Protocol 2';
-        'sshd_ipv4only':      changes => 'set AddressFamily inet';
-        'sshd_root_login':    changes => 'set PermitRootLogin without-password';
-        'sshd_privsep':       changes => 'set UsePrivilegeSeparation yes';
-        'sshd_allow_pubkey':  changes => 'set PubkeyAuthentication yes';
-        'sshd_deny_chresp':   changes => 'set ChallengeResponseAuthentication no';
-        'sshd_deny_plain':    changes => 'set PasswordAuthentication no';
-        'sshd_noempty_pass':  changes => 'set PermitEmptyPasswords no';
-        'sshd_loglevel':      changes => 'set LogLevel VERBOSE';
-        'sshd_internal_sftp':
-            context => "/files${ssh::params::path_sshd_config}/Subsystem",
-            changes => 'set sftp internal-sftp';
+        'sshd_settings':
+            lens    => 'Sshd.lns',
+            context => "/files${ssh::params::path_sshd_config}",
+            incl    => $ssh::params::path_sshd_config,
+            notify  => Service[$ssh::params::service_name],
+            changes => [
+                         'set Protocol 2',
+                         'set AddressFamily inet',
+                         'set LogLevel VERBOSE',
+                         'set UsePrivilegeSeparation yes',
+                         'set PermitEmptyPasswords no',
+                         'set PermitRootLogin without-password',
+                         'set PubkeyAuthentication yes',
+                         'set ChallengeResponseAuthentication no',
+                         'set PasswordAuthentication no',
+                         'set Subsystem/sftp internal-sftp',
+                         "set AuthorizedKeysFile ${ssh::params::path_authorized_keys}/%u",
+                       ];
     }
 }
