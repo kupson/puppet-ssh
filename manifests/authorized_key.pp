fd7af11d3b64c1c96d4f73ae0b05a026374394f8
Rafał Kupka <kupson@kupson.net>
2012-05-22 16:34:52 +0200

    Initial support for ssh::authorized_key
    Kind of hack, uses defined().

---
+++ manifests/authorized_key.pp
@@ -0,0 +1,56 @@
+# Define: ssh::authorized_key
+#
+# Wrapper for ssh_authorized_key resource customized to ssh module.
+#
+# Parameters:
+# 
+#   [*ensure*]      - *present*|absent
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# include ssh
+#
+# ssh::authorized_key {
+#   "demo_access":
+#     ensure => present,
+#     user   => 'demo',
+#     type   => 'rsa',
+#     key    => '<key_data>';
+# }
+#
+define ssh::authorized_key (
+    $ensure = 'present',
+    $user,
+    $type   = 'rsa',
+    $key
+) {
+
+    $target = "${ssh::params::path_authorized_keys}/${user}"
+
+    # How to do it without defined(), custom provider maybe?
+    # It needs to run only once for a user.
+    if !defined(File[$target]) {
+        file {
+            $target:
+                ensure => 'file',
+                owner  => 'root',
+                group  => $user,
+                mode   => '0640';
+        }
+    }
+
+    ssh_authorized_key {
+        $title:
+            ensure  => $ensure,
+            user    => $user,
+            target  => $target,
+            type    => $type,
+            key     => $key,
+            require => File[$target];
+    }
+
+}
