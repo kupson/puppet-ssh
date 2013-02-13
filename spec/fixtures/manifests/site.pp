6f36940b248e6622b43cae44bfc7ae7629640787
floatingatoll <rsoderberg@gmail.com>
2013-02-12 18:02:35 -0800

    puppet-lint whitespace cleanups

--- spec/fixtures/manifests/site.pp
+++ spec/fixtures/manifests/site.pp
@@ -2,24 +2,26 @@ node 'localhost.localdomain' {
     include ssh
     include ssh::client::allenv
 
-    user {'thedummyuser':
-        ensure => present,
+    user {
+        'thedummyuser':
+            ensure => present;
     }
 
-    ssh_authorized_key {'root@localhost.localdomain':
-        key => 'AAAA',
-        type    => 'rsa',
-        user    => 'root',
-        provider => 'parsed_systemdir',
-        require => User['thedummyuser'];
+    ssh_authorized_key {
+        'root@localhost.localdomain':
+            key      => 'AAAA',
+            type     => 'rsa',
+            user     => 'root',
+            provider => 'parsed_systemdir',
+            require  => User['thedummyuser'];
     }  
 
-    ssh_authorized_key {'thedummyuser@localhost.localdomain':
-        key => 'AAAA',
-        type    => 'rsa',
-        user    => 'root',
-        provider => 'parsed_systemdir',
-        require => User['thedummyuser'];
+    ssh_authorized_key {
+        'thedummyuser@localhost.localdomain':
+            key      => 'AAAA',
+            type     => 'rsa',
+            user     => 'root',
+            provider => 'parsed_systemdir',
+            require  => User['thedummyuser'];
     }  
-
-}    
+}
